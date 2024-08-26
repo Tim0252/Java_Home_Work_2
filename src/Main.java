ff14260db9d62a0174f5d411eb6c9259be2fba1f
Artiom Net <neartiom@yande.ru>
2024-08-26 20:12:53 +0400

    done

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 13676; // Стоимость билета
+        int pieces = 20; // Рублей за 1 милю
+
+        System.out.println((ticket/pieces) + " Бонусов начиленно");
+
+    }
+}
